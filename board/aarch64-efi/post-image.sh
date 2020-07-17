be7593a628750aedfd6ebd78291d0004aa15550a
Dick Olsson <hi@senzilla.io>
2020-07-17 18:42:59 +0200

    configs/aarch64_efi_defconfig: build the EDK2 firmware from source
    Prior to this, you had to manually download a pre-built EDK2 flash
    device image (QEMU_EFI.fd) in order to boot this configuration with
    QEMU. Now, the configuration is building EDK2 from source.
    
    Signed-off-by: Dick Olsson <hi@senzilla.io>
    
    ---
    
    Revision 4:
    
     * Simplified resizing of the flash images with the trucate command
    
    Revision 3:
    
     * post-image.sh will now provide resized flash devices
     * readme.txt suggests pflash instead of bios
    
    Revision 2:
    
     * Explicitly added the default EDK2 platform to defconfig

--- board/aarch64-efi/post-image.sh
+++ board/aarch64-efi/post-image.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 BOARD_DIR="$(dirname $0)"
 
 cp -f ${BOARD_DIR}/grub.cfg ${BINARIES_DIR}/efi-part/EFI/BOOT/grub.cfg
+
+# The QEMU virt machine expects flash devices to be 64M.
+truncate -s 64M "${BINARIES_DIR}/QEMU_EFI.fd"
+truncate -s 64M "${BINARIES_DIR}/QEMU_VARS.fd"
