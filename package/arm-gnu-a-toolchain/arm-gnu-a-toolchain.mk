c5491ade7ef648d7447bf7a304b4c1f661b8dee6
Dick Olsson <hi@senzilla.io>
2021-01-10 12:31:12 +0000

    packages/arm-gnu-toolchain: also allow aarch64 hosted toolchain
    Aarch64 hosts can now also use the ARM GNU Toolchain for Cortex-A family
    of processors. This is required for Aarch64 hosts when building ATF for
    certain platforms, such as the rockpro64.
    
    Signed-off-by: Dick Olsson <hi@senzilla.io>

--- package/arm-gnu-a-toolchain/arm-gnu-a-toolchain.mk
+++ package/arm-gnu-a-toolchain/arm-gnu-a-toolchain.mk
@@ -6,7 +6,7 @@
 
 ARM_GNU_A_TOOLCHAIN_VERSION = 9.2-2019.12
 ARM_GNU_A_TOOLCHAIN_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/$(ARM_GNU_A_TOOLCHAIN_VERSION)/binrel
-ARM_GNU_A_TOOLCHAIN_SOURCE = gcc-arm-$(ARM_GNU_A_TOOLCHAIN_VERSION)-x86_64-arm-none-eabi.tar.xz
+ARM_GNU_A_TOOLCHAIN_SOURCE = gcc-arm-$(ARM_GNU_A_TOOLCHAIN_VERSION)-$(HOSTARCH)-arm-none-eabi.tar.xz
 ARM_GNU_A_TOOLCHAIN_LICENSE = GPL-3.0+
 
 HOST_ARM_GNU_A_TOOLCHAIN_INSTALL_DIR = $(HOST_DIR)/opt/gcc-arm-none-eabi
